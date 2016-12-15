3bdc6793d14daf515e289315b8e1d3cde0aa9684
Sdraje <alfmarmil@gmail.com>
2016-12-15 16:38:00 +0000

    changing API domain

--- src/js/constants/api.js
+++ src/js/constants/api.js
@@ -1,4 +1,3 @@
 angular
   .module('discovereel')
-  .constant('API', `http://localhost:3000/api`);
-  // .constant("API", `https://watchedornot-api.herokuapp.com/api`);
+  .constant('API', `https://watchedornot-api.herokuapp.com/api`);
